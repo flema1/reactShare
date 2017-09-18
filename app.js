81288d951eb45b30c2d22970931d86db8d1ebe1b
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-17 21:12:18 -0400

    removed comments

--- app.js
+++ app.js
@@ -40,40 +40,71 @@ app.listen(PORT, function() {
 
 
 
-app.get('/', (req, res) => {
-  res.send('We are live!');
-});
+// app.get('/', (req, res) => {
+//   res.send('We are live!');
+// });
 
-const reactShareRouter = require('./routes/reactShare-routes');
-app.use('/rShare', reactShareRouter);
-
-
-
-app.use('*', (req, res) => {
-  res.status(400).json({
-    message: 'Endpoint not found!',
-  });
-});
+// const reactShareRouter = require('./routes/reactShare-routes');
+// app.use('/rShare', reactShareRouter);
 
 
+
+// app.use('*', (req, res) => {
+//   res.status(400).json({
+//     message: 'Endpoint not found!',
+//   });
+// });
+
+
+// var server = require('http').createServer(app);
+// var io = require('socket.io')(server);
+
+
+// io.on('connection', function(socket){
+//   console.log("new connection from " + socket.id);
+  
+//   io.on("send", function(event) {
+//     console.log("got an event!");
+//     console.log(event);
+//     socket.broadcast.emit("message-from-friend", event);
+//   });
+
+//   //  socket.on('disconnect', () => {
+//   //   console.log('user disconnected');
+//   // });
+// });
+
+// server.listen(3001);
+
+//timestamp 
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(3001);
-io.on('connection', function(socket) {
-  console.log("new connection from " + socket.id);
-  socket.on("send", function(event) {
+
+io.on('connection', function(client){
+  console.log("new connection from " + client.id);
+
+  //  client.on('subscribeToTimer', (interval) => {
+  //  console.log('client is subscribing to timer with interval ', interval);
+
+
+  //   setInterval(() => {
+  //     client.emit('timer', new Date());
+  //   }, interval);
+
+  // });
+  //  socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
+
+client.on("send", function(event) {
     console.log("got an event!");
     console.log(event);
-    socket.broadcast.emit("message-from-friend", event);
+    client.broadcast.to(event.id).emit("message-from-friend", event);
   });
+
+
 });
 
-
-
-
-
-
-
-
+server.listen(3001);
