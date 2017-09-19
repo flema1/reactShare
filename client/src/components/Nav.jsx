d8bfed2dee16fbeca4f541779fcc9b3ef1d2eb45
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-18 20:57:06 -0400

    added nav component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import Auth from '../modules/Auth';
+
+const Nav = (props) => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+              <Link to="/room">Room</Link>
+            </li>
+        </ul>
+        {!Auth.isUserAuthenticated() ? (
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <span className="logout" onClick={props.logoutUser}>Log Out</span>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
