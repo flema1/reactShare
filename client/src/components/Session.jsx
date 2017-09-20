3fc1abdbde5bd9bf2be34414df600290a96c9739
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-20 11:00:44 -0400

    added class names

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -7,14 +7,11 @@ import { Link } from 'react-router-dom';
 const Session = (props) => {
   var code= props.session.code;
   return (
-    <div >
-
-      <h2>{props.session.id}</h2>
-      {/*<h2>{props.session.code}</h2>*/}
-        <DisplaySaved  inputHTML={ code }/>
-    
-      <Link to={`/session/${props.session.id}`}>See More</Link>
-    </div>
+          <div className="session-inner">
+              <h2>{props.session.id}</h2>
+              <DisplaySaved  inputHTML={ code }/>
+              <Link id ="see-more" to={`/session/${props.session.id}`}>See More</Link>
+          </div>
   )
 }
 
