a76111a860939952b3a2ef1a436a2203d3bfe384
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-20 10:57:43 -0400

    removed commented code

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom'
+import axios from 'axios';
+
+import Auth from './modules/Auth';
+import Nav from './components/Nav';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Room from './components/Room';
+import Home from './components/Home'
+import SessionList from './components/SessionList';
+import SessionSingle from './components/SessionSingle';
+import SessionEditForm from './components/SessionEditForm';
+import ContentEditable from './components/ContentEditable'
+import Display from './components/Display';
+
 import {
   BrowserRouter as
   Router,
@@ -9,22 +23,9 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import axios from 'axios';
-import Auth from './modules/Auth';
-import Nav from './components/Nav';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-import Room from './components/Room';
-import SessionList from './components/SessionList';
 
-import SessionSingle from './components/SessionSingle';
-// import SessionAddForm from './components/SessionAddForm';
-import SessionEditForm from './components/SessionEditForm';
-
-import ContentEditable from './components/ContentEditable'
-import Display from './components/Display'
 import io from 'socket.io-client';
-    var __lastHTML="null";
+var __lastHTML="null";
 
 
 
@@ -37,8 +38,6 @@ class App extends Component {
     this.state = {
       auth: Auth.isUserAuthenticated(),
       shouldFireRedirect: false,
-      // form fields
-    
       loginUserName: '',
       loginPassword: '',
       registerUserName: '',
@@ -46,18 +45,12 @@ class App extends Component {
       registerEmail: '',
       registerName: '',
       username:'',
-      // apiData:[],
-      // apiDataLoaded: false,
-      // _redirect: false,
-      // currentPage:'/'
-
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
     this.logoutUser = this.logoutUser.bind(this);
     this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
-    //this.handleClickShow = this.handleClickShow.bind(this);
     this.resetFireRedirect = this.resetFireRedirect.bind(this);
   }
  
@@ -65,39 +58,8 @@ class App extends Component {
     window.addEventListener('load', this.handleLoad);
  }
 
-//  handleLoad() {
-//   //alert("bob");
-//  }
- //onChange(event){
- 
-    //console.log( "event.target.value")
-    //this.setState({html: event.target.value});
-    //if (this.state.html!=__lastHTML){
-    //this.socket.emit('share', {incoming:event.target.value, current:this.state.html})
-    //  __lastHTML=this.state.html;
-    //  alert( __lastHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-    //}
-    
-   // console.log (this.state.html + "www")
-//}
 
-
-
-// handleClick(){
-//         let textArea = document.querySelector("textarea");
-//         var textToSend = textArea.value;
-//         console.log("sending");
-//         this.socket.emit('send', textToSend)
-//         //  messenger(textToSend);       
-// }
-// code(){
-//   this.setState({
-//     finalHTML:this.state.html+ this.state.shared_code
-//   })
-   
-// }
 handleInputChange(e) {
-
     const name = e.target.name;
     const value = e.target.value;
     this.setState({
@@ -116,7 +78,7 @@ handleInputChange(e) {
         Auth.authenticateToken(res.data.token);
         this.setState({
           auth: Auth.isUserAuthenticated(),
-          username: res.data ,
+          username: res.data.username,
           loginUserName: '',
           loginUserPassword: '',
         })
@@ -136,7 +98,7 @@ handleInputChange(e) {
       this.state.registerName,   
 
     )
-    axios.post(/*'/users'*/ 'auth/register', {
+    axios.post('auth/register', {
       user: {
       username: this.state.registerUserName,
       email: this.state.registerEmail,
@@ -187,99 +149,78 @@ handleInputChange(e) {
     })
   }
 
-// handleClickShow(){
-//  console.log("show show");
-//   axios.get('/rShare/show/100')
-//   .then((res) => {
-//     this.setState({
-//       apiData:res.data,
-//       apiDataLoaded: true,
-//       currentPage: 'results',
-//       _redirect: true
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// }
-
   render(){
     return (
-         <Router>
-      <div className="App">
-          <Nav logoutUser={this.logoutUser} />
-          <Route exact path="/login"
-            render={() =>
-              !this.state.auth ? (
-                <LoginForm
-                  auth={this.state.auth}
-                  loginUserName={this.state.loginUsername}
-                  loginPassword={this.state.loginPassword}
-                  handleInputChange={this.handleInputChange}
-                  handleLoginSubmit={this.handleLoginSubmit}
-                />
-              ) : (
-                <Redirect to="/room" component={Room}/>
-              )}
-          />
-          <Route
-            exact
-            path="/room"
-            render={() =>
-              this.state.auth ? (
-                <Room shouldFireRedirect={this.state.shouldFireRedirect}
-                handleClickShow={this.handleClickShow}
-                />
-              ) : (
-                <Redirect to="/login" />
-              )}
-          />
-            {/*<Route exact path="/session" component={Session} />*/}
-            <Route exact path="/session/:id" component={SessionSingle} />
-            {/*<Route exact path="/add" component={SessionAddForm} />*/}
-            <Route exact path="/edit/:id" component={SessionEditForm} />
-          <Route
-            exact
-            path="/saved"
-            render={() =>
-              this.state.auth ? (
-                <SessionList />
-              ) : (
-                <Redirect to="/login" />
-              )}
-          />
+      <Router>
+          <div className="App">
+              <Nav logoutUser={this.logoutUser} />
+                {/*Logged in as: {this.state.username}*/}
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/login"
+                render={() =>
+                  !this.state.auth ? (
+                    <LoginForm
+                      auth={this.state.auth}
+                      loginUserName={this.state.loginUsername}
+                      loginPassword={this.state.loginPassword}
+                      handleInputChange={this.handleInputChange}
+                      handleLoginSubmit={this.handleLoginSubmit}
+                    />
+                  ) : (
+                    <Redirect to="/room" component={Room}/>
+                  )}
+              />
+              <Route
+                exact
+                path="/room"
+                render={() =>
+                  this.state.auth ? (
+                    <Room shouldFireRedirect={this.state.shouldFireRedirect}
+                      handleClickShow={this.handleClickShow}
+                      username={this.state.username}  
+                    />
+                  ) : (
+                    <Redirect to="/login" />
+                  )}
+              />
+            
+              <Route exact path="/session/:id" component={SessionSingle} />
+              <Route exact path="/edit/:id" component={SessionEditForm} />
+              <Route
+                exact
+                path="/saved"
+                render={() =>
+                  this.state.auth ? (
+                    <SessionList  username={ this.state.username}/>
+                  ) : (
+                    <Redirect to="/login" />
+                  )}
+              />
 
-            <Route
-            exact
-            path="/register"
-            render={() =>
-              !this.state.auth ? (
-                <RegisterForm
-                  auth={this.state.auth}
-                  registerUserName={this.state.registerUsername}
-                  registerPassword={this.state.registerPassword}
-                  registerEmail={this.state.registerEmail}
-                  registerName={this.state.registerName}
-                  handleInputChange={this.handleInputChange}
-                  handleRegisterSubmit={this.handleRegisterSubmit}
-                />
-              ) : (
-                <Redirect to="/dash" />
-              )}
-          />
-      
-    
-          {/*This is your username: {this.state.loginUserName}*/}
-      </div>
-         </Router>
+              <Route
+                exact
+                path="/register"
+                render={() =>
+                  !this.state.auth ? (
+                    <RegisterForm
+                      auth={this.state.auth}
+                      registerUserName={this.state.registerUsername}
+                      registerPassword={this.state.registerPassword}
+                      registerEmail={this.state.registerEmail}
+                      registerName={this.state.registerName}
+                      handleInputChange={this.handleInputChange}
+                      handleRegisterSubmit={this.handleRegisterSubmit}
+                    />
+                  ) : (
+                    <Redirect to="/dash" />
+                  )}
+              />
+            
+          </div>
+    </Router>
     );
   }
 }
-
-
-
-
 
 
 export default App;
