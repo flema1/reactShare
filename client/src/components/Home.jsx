31535525b882c83687727a3e9d4bf18e3c467613
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-20 10:59:50 -0400

    added home page component

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+
+constructor(){
+    super();
+   
+  }
+
+  render(){
+    return (
+      <div className="home">
+      </div>
+    )
+  }
+
+}
+export default Home 
