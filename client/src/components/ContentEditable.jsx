2d14681679819f70aeb489ce405bd0f4736381c5
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-09-20 10:58:21 -0400

    removed commented code

--- client/src/components/ContentEditable.jsx
+++ client/src/components/ContentEditable.jsx
@@ -5,7 +5,7 @@ class ContentEditable extends Component {
   constructor() {
     super();
     this.state = {
-         playing: false,
+        //  playing: false,
     }
   
     this.emitChange = this.emitChange.bind(this);
@@ -15,7 +15,7 @@ class ContentEditable extends Component {
     window.addEventListener('load', this.handleLoad);
     const element = ReactDOM.findDOMNode(this);
     console.log(element);
-    //alert(element);
+
  }
  
 
@@ -29,35 +29,14 @@ class ContentEditable extends Component {
 
     emitChange(){
         var html = ReactDOM.findDOMNode(this).innerHTML;
-        // var d = document.createElement('h1').innerHTML
-        // d= ReactDOM.findDOMNode(this)
-        // console.log(d.firstChild);
-        // //var p= document.createElement(d.firstChild)
-        // //console.log (html.toHtmlObject);
-        // // var children = element.childNodes;
-        // var children = d.childNodes;
-        // children.forEach(function(item){
-        // console.log(item.firstChild);
-        // });
-        // console.log (ReactDOM.findDOMNode(this).children)
-
         if (this.props.onChange && html !== this.lastHtml) {
             console.log (html); 
-
-
-//              var wrapper= document.createElement('div');
-// wrapper.innerHTML= html;
-// var div= wrapper.childNodes
-
-
             this.props.onChange({
                 target: {
                     value: html
                 }
             });
         }
-
-
         this.lastHtml = html;
         console.log (this.lastHtml + "the last hmtl")
         console.log (typeof this.lastHtml)
@@ -78,6 +57,7 @@ class ContentEditable extends Component {
  
 
 export default ContentEditable;
+
 /*Sources: 
  https://stackoverflow.com/questions/29527309/react-0-13-this-getdomnode-equivalent-to-react-finddomnode
 https://gist.github.com/girishso/9876306 (contenteditable basecode)
